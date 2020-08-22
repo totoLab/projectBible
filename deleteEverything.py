c2d68cdf0ced5d9a2395c1d822f48e19e8adbff6
toto <antoniolabate19@gmail.com>
2020-08-23 00:41:08 +0200

    script now changes title of the html file

--- deleteEverything.py
+++ deleteEverything.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import re
+import fileinput
 
 def deleteFromTo(dir, path):
     for j in range(1, len(dir)+1):
@@ -29,14 +30,14 @@ def removeLines(logfile, word, outfile):
 def removeOldFiles(oldFile):
     os.remove("{}".format(oldFile))
 
-def removeHtmlJunk(fileToClean, outfile, regex):
-    with open (fileToClean, 'r' ) as f:
+def removeHtmlJunk(fileToModify, outfile, regex):
+    with open (fileToModify, 'r' ) as f:
         content = f.read()
         content_new = re.sub(regex[0], regex[1], content, flags = re.M)
         with open(outfile, 'w') as out:
             out.write(content_new)
 
-    removeOldFiles(fileToClean)
+    removeOldFiles(fileToModify)
         
 
 def copyAndRenamePattern(path, bookName):
@@ -45,29 +46,27 @@ def copyAndRenamePattern(path, bookName):
     return bookName + ".html"
 
 #TODO test, copied from stackoverflow
-def replaceContent(title, actualVerses):
-    toSub = ['title', 'actualVerses']
-    substitutions = [title, actualVerses]
-    for i in range(2):
-        with open(title + ".html", 'r+') as f:
-            text = f.read()
-            text = re.sub(toSub[i], substitutions, text)
-            f.seek(0)
-            f.write(text)
-            f.truncate()
+def replaceContent(path, bookName):
+    titleOfTheDoc = ['bookTitle', bookName]
+    #parsedVerses = ['actualVerses', 0]
+    #regex = [titleOfTheDoc, parsedVerses]
+    #for i in range(2):
+    removeHtmlJunk("{}{}".format(path, bookName), "{}test{}".format(path, bookName), titleOfTheDoc) #add in this case
+        
 
 #TODO
 def copyVersesToFinalFiles(bookName, verses):
     print("Copied {}".format(bookName))
 
-path = "/home/antonio/Documents/VScode/webDev/testProjectBible/bibbiaHtmlTest/"
+path1 = "/home/antonio/Documents/VScode/webDev/testProjectBible/bibbiaHtmlTest/"
 path2 = "/home/antonio/Documents/VScode/webDev/testProjectBible/example.html"
+path3 = "/home/antonio/Documents/VScode/webDev/newProjectBible/"
 arrayDir = ['1.Genesi','2.Esodo','3.Levitico','4.Numeri','5.Deuteronomio','6.Giosuè','7.Giudici','8.Rut','9.Samuele1','10.Samuele2','11.Re1','12.Re2','13.Cronache1','14.Cronache2','15.Esdra','16.Neemia','17.Ester','18.Giobbe','19.Salmi','20.Proverbi','21.Ecclesiaste','22.CanticodeiCantici','23.Isaia','24.Geremia','25.Lamentazioni','26.Ezechiele','27.Daniele','28.Osea','29.Gioele','30.Amos','31.Abdia','32.Giona','33.Michea','34.Naum','35.Abacuc','36.Sofonia','37.Aggeo','38.Zaccaria','39.Malachia','40.Matteo','41.Marco','42.Luca','43.Giovanni','44.Atti','45.Romani','46.Corinzi1','47.Corinzi2','48.Galati','49.Efesini','50.Filippesi','51.Colossesi1','52.Colossesi2','53.Tessalonicesi1','54.Tessalonicesi2','55.Timoteo','56.Tito','57.Filemone','58.Ebrei','59.Giacomo','60.Pietro1','61.Pietro2','62.Giovanni1','63.Giovanni2','64.Giovanni3','65.Giuda','66.Apocalisse']
 def mainFunc():
-    deleteFromTo(arrayDir, path)
-    #for i in range(len(arrayDir)):
-        #bookName = copyAndRenamePattern(path2, arrayDir[i]) #copy example.html and rename with a bookname
-        #replaceContent(bookName, versesFromFiles)
+    deleteFromTo(arrayDir, path1)
+    for i in range(1):#len(arrayDir)):
+        bookName = copyAndRenamePattern(path2, arrayDir[i]) #copy example.html and rename with a bookname
+        replaceContent(path3, bookName)
 
 
 mainFunc()
